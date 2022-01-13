32795b50eec036f499a8ba10f902b0bafcd83e2b
michaelmontalbano <michael.montalbano@ou.edu>
2022-01-13 12:24:31 -0600

    fixing the repo, i hope

--- single.sh
+++ single.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-#SBATCH --job-name=ExtractionTest
+#SBATCH --job-name=ExtractCropConv
 #SBATCH -p swat_plus
 #SBATCH -n 1
 #SBATCH --chdir /condo/swatwork/mcmontalbano/MYRORSS/myrorss-deep-learning
 #SBATCH -t 01:00:00
-source ~fagg/pythonenv/tensorflow/bin/activate
+
+source ~/tensorflow/bin/activate
 python extract.py 
 
 
