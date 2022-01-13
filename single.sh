a84d08e33f4ab75054c2c4f6a8fc81021a091a98
michael.montalbano <michael.montalbano@ou.edu>
2022-01-13 10:25:42 -0600

    activate python environment

--- single.sh
+++ single.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-#SBATCH --job-name=ExtractionTest
+#SBATCH --job-name=ExtractCropConv
 #SBATCH -p swat_plus
 #SBATCH -n 1
+#SBATCH --output=/condo/swatwork/mcmontalbano/MYRORRS/myrorss-deep-learning
 #SBATCH --chdir /condo/swatwork/mcmontalbano/MYRORSS/myrorss-deep-learning
 #SBATCH -t 01:00:00
-source ~fagg/pythonenv/tensorflow/bin/activate
+source tensorflow/bin/activate
 python extract.py 
 
 
