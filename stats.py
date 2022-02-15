36715c8e38eaf8e06af10459e083d9be86b3d1de
michaelmontalbano <michael.montalbano@ou.edu>
2022-02-15 16:09:59 -0600

    Added check_missing() function for MYRORSS/data

--- stats.py
+++ stats.py
@@ -14,6 +14,10 @@ import pandas as pd
 from netCDF4 import Dataset
 from os import walk
 
+multi_fields = ['MergedLLShear_Max_30min','MergedLLShear_Min_30min','MergedMLShear_Max_30min','MergedMLShear_Min_30min','MergedReflectivityQC','MergedReflectivityQCComposite_Max_30min','Reflectivity_0C_Max_30min','Reflectivity_-10C_Max_30min','Reflectivity_-20C_Max_30min','target_MESH_Max_30min']
+NSE_fields = ['MeanShear_0-6km', 'MUCAPE', 'ShearVectorMag_0-1km', 'ShearVectorMag_0-3km', 'ShearVectorMag_0-6km', 'SRFlow_0-2kmAGL', 'SRFlow_4-6kmAGL', 'SRHelicity0-1km', 'SRHelicity0-2km', 'SRHelicity0-3km', 'UWindMean0-6km', 'VWindMean0-6km', 'Heightof0C','Heightof-20C','Heightof-40C']
+products = multi_fields + NSE_fields
+
 DATA_HOME = '/condo/swatcommon/common/myrorss'
 TRAINING_HOME = '/condo/swatwork/mcmontalbano/MYRORSS/data'
 HOME_HOME = '/condo/swatwork/mcmontalbano/MYRORSS/myrorss-deep-learning'
@@ -133,4 +137,58 @@ def load_npy(prefix='outs'):
     data = np.concatenate(data, axis=0)
     return data
 
-
+# check if any of files in storm directory are missing
+#
+def check_storm(date):
+    """
+    Purpose: check if storm is missing
+    Returns:
+        @param number of missing cases
+        @param number of total cases
+    """
+    storms_dir = '{}/{}/{}'.format(TRAINING_HOME,date[:4],date)
+    storms = sorted(os.listdir(storms_dir))
+    df = pd.DataFrame(columns=['storm','missing'])
+    for storm in storms:
+        if storm[:5] == 'storm' and storm[:6] != 'storms':
+            missing = False
+            n_missing = 0
+            for p in products:
+                if p in NSE_fields:
+                    # check if the NSE/product/.netcdf file is missing
+                    file_list = os.listdir('{}/{}/NSE/{}'.format(storms_dir,storm,p))
+                    if len(file_list) == 0:
+                        missing = True
+                        # note that p is missing in a pandas database
+                        n_missing+=1
+                        break
+                if p == 'MergedReflectivityQC':
+                    # check if any of the MergedReflectivityQC files are missing
+                    degrees = os.listdir('{}/{}/MergedReflectivityQC/'.format(storms_dir, storm))
+                    for degree in degrees: # check 
+                        if len(os.listdir('{}/{}/MergedReflectivityQC/{}'.format(storms_dir, storm, degree))) == 0:
+                            missing = True
+                            n_missing+=1
+                            break
+                if p == 'target_MESH_Max_30min':
+                    # check if any of the target_MESH_Max_30min files are missing
+                    targ_dirs = os.listdir('{}/{}/target_MESH_Max_30min/'.format(storms_dir, storm))
+                    if 'MESH_Max_30min' not in targ_dirs:
+                        missing = True
+                        n_missing+=1
+                        break
+                    else:
+                        if len(os.listdir('{}/{}/target_MESH_Max_30min/MESH_Max_30min'.format(storms_dir, storm))) == 0:
+                            missing = True
+                            n_missing+=1
+                            break
+                if p in multi_fields and p != 'target_MESH_Max_30min':
+                    dummy_dir = os.listdir('{}/{}/{}'.format(storms_dir, storm, p))[0]
+                    if len(os.listdir('{}/{}/{}/{}'.format(storms_dir, storm, p, dummy_dir))) == 0:
+                        missing = True
+                        n_missing+=1
+                        break
+                n_missing+=1
+            df = df.append({'storm':storm, 'missing': n_missing}, ignore_index=True)
+                    # check if any of the multi_fields files are missing                 
+    return df
