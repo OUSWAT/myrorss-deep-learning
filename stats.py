8633793e60cbc8ee66d2bf1c62762f285e34ccea
michaelmontalbano <michael.montalbano@ou.edu>
2022-04-12 09:22:42 -0500

    Added main loop to read in models and output basic POD/FAR/CSI

--- stats.py
+++ stats.py
@@ -3,40 +3,67 @@
 # Author: Michael Montalbano
 #
 # TODO:
-#       1. stats() funct that returns POD, FAR, CSI, etc (Roebber 2008)
 #       2. Taylor (2001) performance graph
-#	2. MED() for mean_error_distance (use networkx) 
-#       3. Check for dependence on lat,lon
 
-import pickle, sys, os, glob
+import pickle
+import sys
+import os
+import glob
 import numpy as np
 import pandas as pd
-from netCDF4 import Dataset
 from os import walk
 from collections import Counter
+import util
 
-multi_fields = ['MergedLLShear_Max_30min','MergedLLShear_Min_30min','MergedMLShear_Max_30min','MergedMLShear_Min_30min','MergedReflectivityQC','MergedReflectivityQCComposite_Max_30min','Reflectivity_0C_Max_30min','Reflectivity_-10C_Max_30min','Reflectivity_-20C_Max_30min','target_MESH_Max_30min']
-NSE_fields = ['MeanShear_0-6km', 'MUCAPE', 'ShearVectorMag_0-1km', 'ShearVectorMag_0-3km', 'ShearVectorMag_0-6km', 'SRFlow_0-2kmAGL', 'SRFlow_4-6kmAGL', 'SRHelicity0-1km', 'SRHelicity0-2km', 'SRHelicity0-3km', 'UWindMean0-6km', 'VWindMean0-6km', 'Heightof0C','Heightof-20C','Heightof-40C']
+multi_fields = [
+    'MergedLLShear_Max_30min',
+    'MergedLLShear_Min_30min',
+    'MergedMLShear_Max_30min',
+    'MergedMLShear_Min_30min',
+    'MergedReflectivityQC',
+    'MergedReflectivityQCComposite_Max_30min',
+    'Reflectivity_0C_Max_30min',
+    'Reflectivity_-10C_Max_30min',
+    'Reflectivity_-20C_Max_30min',
+    'target_MESH_Max_30min']
+NSE_fields = [
+    'MeanShear_0-6km',
+    'MUCAPE',
+    'ShearVectorMag_0-1km',
+    'ShearVectorMag_0-3km',
+    'ShearVectorMag_0-6km',
+    'SRFlow_0-2kmAGL',
+    'SRFlow_4-6kmAGL',
+    'SRHelicity0-1km',
+    'SRHelicity0-2km',
+    'SRHelicity0-3km',
+    'UWindMean0-6km',
+    'VWindMean0-6km',
+    'Heightof0C',
+    'Heightof-20C',
+    'Heightof-40C']
 products = multi_fields + NSE_fields
 
 DATA_HOME = '/condo/swatcommon/common/myrorss'
 TRAINING_HOME = '/condo/swatwork/mcmontalbano/MYRORSS/data'
 HOME_HOME = '/condo/swatwork/mcmontalbano/MYRORSS/myrorss-deep-learning'
 
-def stats(y_true, y_pred,scaler):
+
+def stats(r, scaler):
     # return metrics for finding POD, FAR, CSI, etc (Roebber 2008)
-    container, c = binary_accuracy(y_true, y_pred, scaler)
-    correct, total, TP, FP, TN, FN, events = [x for x in container]   
-    POD = TP/(TP + FN)
-    FAR = FP/(TN + FP) # SR = 1 - FAR 
-    bias = (TP + FP)/(TP + FN)
+    container = binary_accuracy(r, scaler)
+    correct, total, TP, FP, TN, FN, events = [x for x in container]
+    POD = TP / (TP + FN)
+    FAR = FP / (TP + FP)  # SR = 1 - FAR
+    bias = (TP + FP) / (TP + FN)
     CSI = TP / (TP + FP + FN)
     return [POD, FAR, bias, CSI]
 
-def binary_accuracy(y_true,y_pred,scaler):
+
+def binary_accuracy(r, scaler):
     # return metrics for finding POD, FAR, CSI, etc
-    y_true = y_true
-    y_pred = y_pred
+    y_true = r['true_testing']
+    y_pred = r['predict_testing']
 
     # scale data back to mm
     correct = 0
@@ -47,27 +74,16 @@ def binary_accuracy(y_true,y_pred,scaler):
     FN = 0
     events = 0
     for idx, ex in enumerate(y_true):
-        ex = ex.reshape(1, 60*60)
-        ex_pred = y_pred[idx].reshape(1,60*60)
+        ex = ex.reshape(1, 60 * 60)
+        ex_pred = y_pred[idx].reshape(1, 60 * 60)
         ex = scaler.inverse_transform(ex)
         ex_pred = scaler.inverse_transform(ex_pred)
 
         # Count Area Shared Above threshold (20 mm)
         thres = 20
-        true_area = 0
-        pred_area = 0
-        common_area = 0
-        for idx, row in enumerate(ex):
-            for idx2, pixel in enumerate(row):
-                if pixel >= thres:
-                    true_area+=1
-                    if ex_pred[idx][idx2] >= thres:
-                        common_area+=1
-                if ex_pred[idx][idx2] >= thres:
-                    pred_area+=1
-        
+
         # binarize the data
-        ex_pred = np.where(ex_pred < 20, 0, 1) 
+        ex_pred = np.where(ex_pred < 20, 0, 1)
         ex = np.where(ex < 20, 0, 1)
 
         # Compute positives, negatives, etc
@@ -75,58 +91,61 @@ def binary_accuracy(y_true,y_pred,scaler):
             for idx2, pixel in enumerate(row):
                 pred_pix = ex_pred[idx][idx2]
                 if pixel == pred_pix:
-                    correct+=1
+                    correct += 1
                     if pixel == 1:
-                        TP+=1
+                        TP += 1
                 if pixel == 1:
-                    events+=1
+                    events += 1
                 if pixel != pred_pix and pred_pix == 1:
-                    FP +=1
-                if pixel == pred_pix and pred_pix ==0:
-                    TN+=1
+                    FP += 1
+                if pixel == pred_pix and pred_pix == 0:
+                    TN += 1
                 if pixel != pred_pix and pred_pix == 0:
-                    FN+=1
-                total+=1
-    print('correct total TP FP TN FN events, true area    predicted area    commmon area')
-    return [correct, total, TP, FP, TN, FN, events], [true_area, pred_area, common_area]
+                    FN += 1
+                total += 1
+    return [correct, total, TP, FP, TN, FN, events]
+# Build dataframe of days and storm count
 
-# Build dataframe of days and storm count 
+
 def build_df(cases):
     days = cases
     storms = []
     for day in days:
         storm_count = 0
         year = day[:4]
-        storm_path = '{}/{}/{}'.format(TRAINING_HOME,year,day)
+        storm_path = '{}/{}/{}'.format(TRAINING_HOME, year, day)
         subdirs = sorted(os.listdir(storm_path))
         print(subdirs)
-        for subdir in subdirs: # for storm in storms
+        for subdir in subdirs:  # for storm in storms
             if subdir[:5] == 'storm' and subdir[:6] != 'storms':
-                storm_count+=1
+                storm_count += 1
         storms.append(storm_count)
         print(storms)
-    df = pd.DataFrame(data={'days':days,'storms':storms})
+    df = pd.DataFrame(data={'days': days, 'storms': storms})
     return df
 
 # Get the cases in year
-def get_cases(year = '1999'):
+
+
+def get_cases(year='1999'):
     cases = []
-    path = '{}/{}'.format(TRAINING_HOME,year)
+    path = '{}/{}'.format(TRAINING_HOME, year)
     possible_storms = os.listdir(path)
     for storm in possible_storms:
         if storm[:4] == year:
             cases.append(storm[:8])
     return cases
 
+
 def load_npy(prefix='outs'):
     # load npy with prefix and return as single np array (or npy)
-    files = os.listdir('{}/{}'.format(HOME_HOME,'datasets'))
+    files = os.listdir('{}/{}'.format(HOME_HOME, 'datasets'))
     names = []
-    for idx, f in enumerate(files[:-1]): # collect all npys fname prefix
+    for idx, f in enumerate(files[:-1]):  # collect all npys fname prefix
         if f[:2] == prefix[:2]:
             names.append(f)
     # data is a list containing each npy
-    data = [np.load('{}/datasets/{}'.format(HOME_HOME,x)) for x in names]
+    data = [np.load('{}/datasets/{}'.format(HOME_HOME, x)) for x in names]
     # correct :the shape check
     for idx, d in enumerate(data):
         if d.shape[1:3] != (60, 60):
@@ -137,6 +156,8 @@ def load_npy(prefix='outs'):
     return data
 
 # check if any of files in storm directory are missing using glob
+
+
 def check_missing(date):
     """
     Purpose: check if storm is missing
@@ -144,21 +165,29 @@ def check_missing(date):
         @param dataframe for each date
         df attributes: stormID (str), missing (boolean), missing_fields (list)
     """
-    storms_dir = '{}/{}/{}'.format(TRAINING_HOME,date[:4],date) # path to storm directory
-    storms = sorted(os.listdir(storms_dir)) # list of storms
-    df = pd.DataFrame(columns=['storm','missing','miss_fields'])
+    storms_dir = '{}/{}/{}'.format(TRAINING_HOME,
+                                   date[:4], date)  # path to storm directory
+    storms = sorted(os.listdir(storms_dir))  # list of storms
+    df = pd.DataFrame(columns=['storm', 'missing', 'miss_fields'])
     for storm in storms:
         # use glob to check if any of the files are missing
-        dirs = sorted(glob.glob('{}/{}/{}/*'.format(TRAINING_HOME,date[:4],date)), reverse = True) # sort the dirs in reverse order
+        # sort the dirs in reverse order
+        dirs = sorted(glob.glob(
+            '{}/{}/{}/*'.format(TRAINING_HOME, date[:4], date)), reverse=True)
         for d in dirs:
-            if int(d[-1]): # check if the last char is a number
+            if int(d[-1]):  # check if the last char is a number
                 missing, fields = check_storm(d)
                 try:
-                    missing_fields = list((Counter(fields) - Counter(products).elements()))
-                except:
+                    missing_fields = list(
+                        (Counter(fields) - Counter(products).elements()))
+                except BaseException:
                     missing_fields = fields
-                df = df.append({'storm':storm,'missing':missing,'miss_fields':fields}, ignore_index=True)
+                df = df.append({'storm': storm,
+                                'missing': missing,
+                                'miss_fields': fields},
+                               ignore_index=True)
     return df
+
 
 def check_storm(storm_path):
     # given a storm path, check if any of the files are missing
@@ -166,14 +195,36 @@ def check_storm(storm_path):
     # find all files in storm path
     files = []
     fields = []
-    for f in glob.glob('{}/**/*.netcdf'.format(storm_path),recursive = True): # recursively returns all files at any depth ending in .netcdf
+    for f in glob.glob(
+        '{}/**/*.netcdf'.format(storm_path),
+            recursive=True):  # recursively returns all files at any depth ending in .netcdf
         field = f.split('/')[-3]
         if field in fields:
-            continue # if the field is already in the list, skip
+            continue  # if the field is already in the list, skip
         fields.append(field)
         files.append(f)
     if len(files) != 44:
         missing = True
     else:
         missing = False
-    return missing, fields 
+    return missing, fields
+
+
+def main():
+    t1 = 'mse_1000_epochs_50_bs_64a128_filters_0.01_l2_2011_qc_results.pkl'
+    t2 = 'mse_1000_epochs_50_bs_64a128_filters_2011_qc_results.pkl'
+    t3 = 'mse_1000_epochs_50_bs_64a128_filters_0.11_l2_2011_qc_results.pkl'
+    t4 = 'mse_100_epochs_200_bs_2011_qc_results.pkl'
+    r1 = util.open_pickle(t1,'results')
+    r2 =  util.open_pickle(t2,'results')
+    r3 = util.open_pickle(t3,'results')
+    r4 = util.open_pickle(t4,'results')
+    scaler = util.open_pickle('scaler_outs_2011_qc.pkl','scalers')
+    scaler_raw = util.open_pickle('scaler_outs_raw.pkl','scalers')
+    print('POD, FAR, CSI')
+    print(t1,stats(r1,scaler))
+    print(t2,stats(r2,scaler))
+    print(t3,stats(r3,scaler))
+    print(t4,stats(r4,scaler))
+if __name__ == "__main__":
+    main()
