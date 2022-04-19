8719a51315f725ac4449c34e801edbe5bcf817b2
michaelmontalbano <michael.montalbano@ou.edu>
2022-04-18 21:38:37 -0500

    Made it easier to review using the script

--- review.py
+++ review.py
@@ -8,13 +8,14 @@ import pickle
 import sys
 import os
 import glob
+import plotlib
 import numpy as np
 import pandas as pd
 from collections import Counter
-from util import open_pickle as op
 import util
-from stats import stats
+import stats
 from netCDF4 import Dataset
+import matplotlib.pyplot as plt
 
 DATA_HOME = '/condo/swatwork/mcmontalbano/MYRORSS/data/'
 
@@ -22,7 +23,7 @@ DATA_HOME = '/condo/swatwork/mcmontalbano/MYRORSS/data/'
 def get_images(fname, step='testing'):
     # given an fname, open the pickle and return the images of interest
     # @param step - choose the step ('training' or 'testing')
-    pik = op(fname)
+    pik = open_pickle(fname)
     # return the images from the selected step
     return pik['true_{}'.format(step)], pik['predict_{}'.format(step)]
 
@@ -37,11 +38,17 @@ def quick_check(y_true, y_pred, scaler):
         yp = scaler.inverse_transform(yp)
         print(ex.max(), yp.max())
 
-
 def get_interesting_pickles():
-    return glob('results/interest/*.pkl')
+    return glob.glob('results/interest/*.pkl')
 
 # examine the outs
+
+def plot_interesting_pickles():
+    pickles = get_interesting_pickles()
+    for fname in pickles:
+        r = util.open_pickle(fname)
+        plotlib.plot_loss(r['history'],ID='{}_loss'.format(fname)) #plot the loss
+        plt.savefig('most_recent_{}.png'.format(fname.split('/')[-1]))   
 
 
 def summarize(outs):
@@ -82,15 +89,23 @@ def percent_above(field, thres, min_pixels):
 
 def main():
     fname_list = get_interesting_pickles()
+ #   plot_interesting_pickles()
+ #   scaler = util.op('scalers/scaler_outs_2011_80.pkl')
+    for fname in fname_list:
+        r = util.open_pickle(fname)
+        try:
+            print(fname)
+            print(stats.stats(r,threshold=25))
+        except: 
+            print('divide by zero')
     # skip the intersting loop writing for later
-    fname = fname_list[0]
-    res = op(fname)
-    y_true, y_pred = get_images(fname)
-    scaler = op('scaler_MSE_1.pkl')[0]  # replace with fname
+    #fname = fname_list[0]
+    #res = util.open_pickle(fname)
+    #y_true, y_pred = get_images(fname)
+    #scaler = op('scaler_MSE_1.pkl')[0]  # replace with fname
     # returns a list of [POD, FAR, bias, and CSI
-    container = stat(y_true, y_pred, scaler)
-    print(container)
-
+    #container = stat(y_true, y_pred, scaler)
+    #print(container)
 
 if __name__ == '__main__':
     main()
